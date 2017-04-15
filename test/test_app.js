1b7d27f0b6cca6ef4c20a501ea5af1c8e521538c
Robin Adams <robin.adams.78@gmail.com>
2017-04-15 12:08:10 +0200

    Use request instead of superagent

--- test/test_app.js
+++ test/test_app.js
@@ -8,7 +8,7 @@ var request = require('request');
 describe('homepage', function(){
 	it('should respond to GET', function(){
 		request('https://localhost:3000', function(err, res, body) {
-			expect(res.statusCode).to.equal(200);
+			expect(res.status).to.equal(200);
 		});
 	});
 });
