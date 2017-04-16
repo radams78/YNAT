eea0426a67c53de0e3e9691acd9f4588671335e0
Robin Adams <robin.adams.78@gmail.com>
2017-04-16 11:18:58 +0200

    Clean up code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
+
+var TIMETABLE_FILE = "data/timetable.json"
+var TITLE = "You Need a Timetable";
+var HOURS_IN_WEEK = 7 * 24;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'You Need a Timetable' });
+	fs.readFile(TIMETABLE_FILE, 'utf8', function(err, data) {
+		if (err) throw err;
+		
+		var timetable = JSON.parse(data);
+		var unbudgeted = HOURS_IN_WEEK;
+		
+		res.render('index', { 
+			title: TITLE,
+			unbudgeted: unbudgeted,
+			timetable: timetable
+			});
+	});
 });
 
 module.exports = router;
