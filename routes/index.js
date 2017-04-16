8138397e073d8bd82aa45bc9928d2ba79994dccb
Robin Adams <robin.adams.78@gmail.com>
2017-04-16 11:20:27 +0200

    Remove users autogenerated by Express

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
-var TIMETABLE_FILE = "data/timetable.json"
+var TIMETABLE_FILE = "./data/timetable.json"
 var TITLE = "You Need a Timetable";
 var HOURS_IN_WEEK = 7 * 24;
 
