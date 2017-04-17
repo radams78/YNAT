426623348297b3f34674b6722f194693ccc1a429
Robin Adams <robin.adams.78@gmail.com>
2017-04-17 19:57:51 +0200

    Test for readTimetable

--- test/test_timetableio.js
+++ test/test_timetableio.js
@@ -5,5 +5,16 @@
  */
 
 const expect = require('chai').expect;
+const fs = require('fs');
 const timetableio = require('../src/timetableio');
 
+describe('timetableio', () => {
+	it('should read a timetable from a file', (done) => {
+		fs.writeFile(timetableio.FILENAME, '{"Work":10,"Rest":20}', (err) => { // TODO Duplication
+			timetableio.readTimetable((err, ttable) => {
+				expect(ttable.toObject()).to.eql({"Work": 10, "Rest": 20});
+				done();
+			});
+		});
+	});
+});
